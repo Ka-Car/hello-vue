ff08c89617c6e93ca7dc8e181aa0d165caf33c5f
Ka-Car <yangguidong_dev@163.com>
2018-08-03 15:44:17 +0800

    ...

--- start-container.sh
+++ start-container.sh
@@ -1,2 +1,2 @@
 echo start a docker container for hellovue
-docker run --name hello-vue --restart unless-stopped -p 8011:8011 -d k/hellovue
+docker run --name hellovue --restart unless-stopped -p 8011:8011 -d k/hellovue
